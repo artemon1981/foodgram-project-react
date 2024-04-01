8097cc3a16ebbdedacd81937b1648d09f7a41afa
artemon1981 <pavelostrikov1981@gmail.com>
2024-04-01 19:57:40 +0300

    new edit

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -107,7 +107,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR / 'collected_static')
+STATIC_ROOT = BASE_DIR / 'collected_static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
@@ -133,8 +136,7 @@ DJOSER = {
 
 AUTH_USER_MODEL = 'users.User'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
