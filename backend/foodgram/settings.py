375997fde0514d3e113616d94f0a90db01ead50a
artemon1981 <pavelostrikov1981@gmail.com>
2024-04-02 11:19:29 +0300

    global changes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -107,10 +107,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / "collected_static"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
